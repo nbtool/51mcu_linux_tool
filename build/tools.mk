d958862e1c4fa16112af42415809e0b3ee899981
HYLI <beautifulzzzz@qq.com>
2018-10-20 15:17:08 +0800

    修改下结构

--- build/tools.mk
+++ build/tools.mk
@@ -1,7 +1,9 @@
 
-PROJECT_ROOT := ~/Downloads/51mcu_linux_tool/example
-TOOLS_PATH := $(PROJECT_ROOT)/../tool
-HEX2BIN_PATH := $(TOOLS_PATH)/hex2bin-2.5
+PROJECT_ROOT := $(shell cd .. && pwd)
+TOOLS_PATH := $(PROJECT_ROOT)/tool
+BIN_PATH := $(PROJECT_ROOT)/bin
+APP_PATH := $(PROJECT_ROOT)/app
+HEX2BIN_PATH := $(TOOLS_PATH)/Hex2bin-2.5
 SDCC_PATH := $(TOOLS_PATH)/sdcc
 STCFLASH_PATH := $(TOOLS_PATH)/stcflash
 
@@ -16,6 +18,10 @@ tools:
 
 	if [ ! -d $(STCFLASH_PATH) ]; then \
 		unzip $(STCFLASH_PATH).zip -d $(TOOLS_PATH) ; \
+	fi
+
+	if [ ! -d $(BIN_PATH) ]; then \
+		mkdir $(BIN_PATH); \
 	fi
 
 
